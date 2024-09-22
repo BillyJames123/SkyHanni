6deae6f76f824627e048fa27282bfb78e355766a
Billyjames123 <Jack.jenkinson@icloud.com>
2024-09-22 09:31:26 +0100

    done

---
+++ src/main/java/at/hannibal2/skyhanni/features/rift/area/lagoon/LagoonCaveParkour.kt
@@ -0,0 +1,82 @@
+package at.hannibal2.skyhanni.features.rift.area.lagoon
+
+import at.hannibal2.skyhanni.data.jsonobjects.repo.ParkourJson
+import at.hannibal2.skyhanni.events.CheckRenderEntityEvent
+import at.hannibal2.skyhanni.events.ConfigLoadEvent
+import at.hannibal2.skyhanni.events.LorenzChatEvent
+import at.hannibal2.skyhanni.events.LorenzRenderWorldEvent
+import at.hannibal2.skyhanni.events.RepositoryReloadEvent
+import at.hannibal2.skyhanni.features.rift.RiftAPI
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.ColorUtils.toChromaColor
+import at.hannibal2.skyhanni.utils.ConditionalUtils
+import at.hannibal2.skyhanni.utils.LorenzUtils
+import at.hannibal2.skyhanni.utils.ParkourHelper
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+@SkyHanniModule
+object LagoonCaveParkour {
+
+    private val config get() = RiftAPI.config.area.mirrorverse.lavaMazeConfig
+    private var parkourHelper: ParkourHelper? = null
+
+    @SubscribeEvent
+    fun onRepoReload(event: RepositoryReloadEvent) {
+        val data = event.getConstant<ParkourJson>("parkour/LagoonCaveParkour")
+        parkourHelper = ParkourHelper(
+            data.locations,
+            data.shortCuts,
+            platformSize = 2.0,
+            detectionRange = 2.0
+        )
+        updateConfig()
+    }
+
+    @SubscribeEvent
+    fun onCheckRender(event: CheckRenderEntityEvent<*>) {
+        if (!isEnabled()) return
+        if (!config.hidePlayers) return
+
+        parkourHelper?.let {
+            if (it.inParkour()) {
+                event.cancel()
+            }
+        }
+    }
+
+    @SubscribeEvent
+    fun onChat(event: LorenzChatEvent) {
+        if (!isEnabled()) return
+
+        if (event.message == "§cIts toxicity somehow teleported you back!") {
+            parkourHelper?.reset()
+        }
+        if (event.message == "§cIts anger somehow teleported you back!") {
+            parkourHelper?.reset()
+        }
+    }
+
+    @SubscribeEvent
+    fun onConfigLoad(event: ConfigLoadEvent) {
+        ConditionalUtils.onToggle(config.rainbowColor, config.monochromeColor, config.lookAhead) {
+            updateConfig()
+        }
+    }
+
+    private fun updateConfig() {
+        parkourHelper?.run {
+            rainbowColor = config.rainbowColor.get()
+            monochromeColor = config.monochromeColor.get().toChromaColor()
+            lookAhead = config.lookAhead.get() + 1
+        }
+    }
+
+    @SubscribeEvent
+    fun onRenderWorld(event: LorenzRenderWorldEvent) {
+        if (!isEnabled()) return
+
+        parkourHelper?.render(event)
+    }
+
+    fun isEnabled() = RiftAPI.inRift() && LorenzUtils.skyBlockArea == "Mirrorverse" && config.enabled
+}
